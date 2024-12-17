0fbdb7a52cea490e88fb095970596055755950c5
AlexS <schwimmeralexander@gmail.com>
2024-12-17 16:23:43 -0700

    Modify TemplateBuilder to create an HTML table

--- VroomQuote/src/TemplateBuilder.java
+++ VroomQuote/src/TemplateBuilder.java
@@ -10,20 +10,25 @@ public class TemplateBuilder {
             templateFile = new File("templates/template.html");
             String line;
             template = new StringBuilder();
-            template.append("<!DOCTYPE html>\n")
-                    .append("<html>\n<head></head>\n")
-                    .append("<body>\n");
+            template.append("<!DOCTYPE html>\n<html>\n<head>\n<title>Dynamic Table</title>\n</head>\n<body>\n");
+            template.append("<table border=\"1\" style=\"width:50%; text-align:left;\">\n");
+            template.append("<tr><th>Column 1</th><th>Column 2</th></tr>\n");
             while(true) {
                 line = scnr.nextLine();
                 if(line.equalsIgnoreCase("end")) {
                     break;
                 }
-                template.append("\t<div>");
+                template.append("\t<tr><td>");
                 String placeholder = String.format("{{%s}}", line);
                 template.append(placeholder);
-                template.append("</div>\n");
+                template.append("</td>\n");
+                template.append("\t<td>");
+                template.append("{{VALUE}}");
+                template.append("</td></tr>\n");
+
             }
-            template.append("</body>\n</html>");
+            template.append("</table></body>\n</html>");
+
         }
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(templateFile))) {
             writer.write(template.toString());
